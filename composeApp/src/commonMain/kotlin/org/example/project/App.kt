4dddae962a4122ee1f9806722b992612a921d49c
LeoAndo <leo.777.info@gmail.com>
2025-09-20 19:48:34 +0900

    Use MaterialExpressiveTheme and add some M3 components
    This commit introduces the `MaterialExpressiveTheme` from Jetpack Compose and incorporates new Material Design 3 (M3) components: `CircularWavyProgressIndicator` and `LinearWavyProgressIndicator`.
    
    Key changes:
    - Replaced `MaterialTheme` with `MaterialExpressiveTheme` in `App.kt`.
    - Added `CircularWavyProgressIndicator` and `LinearWavyProgressIndicator` to the UI.
    - Updated the dependencies in `composeApp/build.gradle.kts` to include `org.jetbrains.compose.material3:material3:1.9.0-alpha04`.
    - Added new image assets to the `capture` directory.

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -8,6 +8,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularWavyProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.LinearWavyProgressIndicator
+import androidx.compose.material3.MaterialExpressiveTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
@@ -19,10 +24,11 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 import kmpmaterialexpressivethemedemo.composeapp.generated.resources.Res
 import kmpmaterialexpressivethemedemo.composeapp.generated.resources.compose_multiplatform
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class, ExperimentalMaterial3Api::class)
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+    MaterialExpressiveTheme {
         var showContent by remember { mutableStateOf(false) }
         Column(
             modifier = Modifier
@@ -31,9 +37,16 @@ fun App() {
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
+
+            // MaterialExpressiveTheme コンポーネントの一部 - START
+            CircularWavyProgressIndicator()
+            LinearWavyProgressIndicator()
+            // END
+
+            Button(
+                content = { Text("show") },
+                onClick = { showContent = !showContent })
+
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
                 Column(
